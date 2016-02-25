82f751d8723465164cbe6150385c56a183643458
Vladimir <v.shtille@gmail.com>
2016-02-25 12:02:35 +0300

    added shtille earth project on MinGW

--- platform/windows/make.bat
+++ platform/windows/make.bat
@@ -1,3 +1,4 @@
 c:\mingw\bin\mingw32-make.exe -f shtille-engine.mk
 c:\mingw\bin\mingw32-make.exe -f testproject.mk
+c:\mingw\bin\mingw32-make.exe -f shtille-earth.mk
 pause
---
+++ platform/windows/shtille-earth.mk
@@ -0,0 +1,35 @@
+# Makefile for windows
+# This makefile has been made for compilation possibility under systems without Visual Studio 2013
+
+SHT_PATH = ../../sht
+SHT_BIN = ../../bin
+
+SOURCES = ../../ShtilleEarth/Source.cpp
+
+INCLUDE = -I$(SHT_PATH)
+
+CC = g++
+CFLAGS = -g -Wall -std=c++11
+CFLAGS += $(INCLUDE)
+LDFLAGS = -s -mwindows
+OBJECTS = $(SOURCES:.cpp=.o)
+TARGET = ShtilleEarth
+TARGET_PATH = ..\..\bin
+EXECUTABLE = $(TARGET).exe
+
+LIBRARIES = \
+	-lstdc++ -lgdi32 \
+	-lShtilleEngine \
+        -lglew -lopengl32 \
+        -lfreetype -ljpeg -lpng -lz
+
+all: $(SOURCES) $(EXECUTABLE)
+	echo All is done!
+	
+$(EXECUTABLE): $(OBJECTS)
+	$(CC) $(LDFLAGS) -o $@ $^ $(LIBRARIES) -L$(SHT_BIN)
+	copy /Y $(EXECUTABLE) $(TARGET_PATH)\$(EXECUTABLE)
+	del /Q $(EXECUTABLE)
+
+%.o : %.cpp
+	$(CC) $(CFLAGS) -c $< -o $@
